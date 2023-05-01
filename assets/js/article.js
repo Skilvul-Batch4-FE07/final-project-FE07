785592f16e2e2d93c08d0395854a8dad9303a585
fitroh <fitrohiza@gmail.com>
2023-05-01 17:11:36 +0700

    Add Detail Artikel

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,15 +1,18 @@
 const url = "https://644e64ed1b4567f4d5866c65.mockapi.io/articles";
 
-const request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    const data = JSON.parse(this.responseText);
-
+fetch(url)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error("Network response was not ok.");
+  })
+  .then((data) => {
     const imageUrl = data[0].image;
 
-    const img = document.querySelector('.card-img-top');
+    const img = document.querySelector(".card-img-top");
     img.src = imageUrl;
-    img.alt = 'Gambar artikel';
+    img.alt = "Gambar artikel";
 
     const title = data[0].title;
     const desc = data[0].description;
@@ -18,13 +21,14 @@ request.onreadystatechange = function () {
     document.querySelector(".card-text").innerHTML = desc;
 
     const dateUpd = data[1].createdAt;
-    const dateOnly = dateUpd.split('T')[0].split('-').slice(0, 3).join('-');
+    const dateOnly = dateUpd.split("T")[0].split("-").slice(0, 3).join("-");
     document.getElementById("date-update").innerHTML = dateOnly;
-  
+
     const titlePilihan = data[1].title;
-    console.log(titlePilihan)
+    console.log(titlePilihan);
     document.getElementById("art-pilihan1").innerHTML = titlePilihan;
-  }
-};
-request.open("GET", url, true);
-request.send();
+  })
+  .catch((error) => {
+    console.error("There was a problem fetching the data:", error);
+  });
+s
