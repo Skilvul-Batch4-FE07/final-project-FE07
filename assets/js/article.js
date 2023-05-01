277f4b5df7c1f9c0289a87a32f5c86e73b81a144
fitroh <fitrohiza@gmail.com>
2023-05-01 19:54:14 +0700

    update call api

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,34 +1,120 @@
 const url = "https://644e64ed1b4567f4d5866c65.mockapi.io/articles";
 
-fetch(url)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Network response was not ok.");
-  })
-  .then((data) => {
-    const imageUrl = data[0].image;
+function createCard(image, title, description) {
+  const card = document.createElement("div");
+  card.classList.add("card", "bg-transparent");
 
-    const img = document.querySelector(".card-img-top");
-    img.src = imageUrl;
-    img.alt = "Gambar artikel";
+  const img = document.createElement("img");
+  img.src = image;
+  img.classList.add("card-img-top");
+  img.alt = "img-artikel";
 
-    const title = data[0].title;
-    const desc = data[0].description;
+  const body = document.createElement("div");
+  body.classList.add("card-body");
 
-    document.querySelector(".card-title").innerHTML = title;
-    document.querySelector(".card-text").innerHTML = desc;
+  const titleH5 = document.createElement("h5");
+  titleH5.classList.add("card-title", "fw-semibold");
+  titleH5.style = "font-size: 18px; padding-bottom: 10px";
+  titleH5.textContent = title;
 
-    const dateUpd = data[1].createdAt;
-    const dateOnly = dateUpd.split("T")[0].split("-").slice(0, 3).join("-");
-    document.getElementById("date-update").innerHTML = dateOnly;
+  const paragraf = document.createElement("p");
+  paragraf.classList.add("card-text", "fw-normal");
+  paragraf.style = "font-size: 14px; color: #696969";
+  paragraf.textContent = description;
 
-    const titlePilihan = data[1].title;
-    console.log(titlePilihan);
-    document.getElementById("art-pilihan1").innerHTML = titlePilihan;
-  })
-  .catch((error) => {
-    console.error("There was a problem fetching the data:", error);
-  });
-s
+  const button = document.createElement("div");
+  button.classList.add("d-flex", "justify-content-end");
+
+  const anchor = document.createElement("a");
+  anchor.classList.add("btn", "py-3", "px-3");
+  anchor.textContent = "Baca Selengkapnya";
+
+  button.appendChild(anchor);
+
+  body.appendChild(titleH5);
+  body.appendChild(paragraf);
+  body.appendChild(button);
+
+  card.appendChild(img);
+  card.appendChild(body);
+
+  console.log(card);
+  return card;
+}
+
+function createCardPilihan(image, createdAt, title) {
+  const row = document.createElement("div");
+  row.classList.add("row", "mb-3");
+
+  const colImg = document.createElement("div");
+  colImg.classList.add("col-md-6");
+
+  const img = document.createElement("img");
+  img.src = image;
+  img.classList.add("img-fluid", "rounded-start");
+  img.alt = "img-artikel";
+  
+  colImg.appendChild(img);
+
+  const colText = document.createElement("div");
+  colText.classList.add("col-md-6");
+
+  const body = document.createElement("div");
+  body.classList.add("card-body");
+
+  const dateSpan = document.createElement("span");
+  dateSpan.classList.add("small", "text-muted");
+  dateSpan.textContent = createdAt
+    .split("T")[0]
+    .split("-")
+    .slice(0, 3)
+    .join("-");
+
+  const titleH5 = document.createElement("h5");
+  titleH5.classList.add("card-title", "fw-bold");
+  titleH5.style.fontSize = "16px";
+  titleH5.textContent = title;
+
+  body.appendChild(dateSpan);
+  body.appendChild(titleH5);
+
+  colText.appendChild(body);
+
+  row.appendChild(colImg);
+  row.appendChild(colText);
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.appendChild(row);
+
+  return card;
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sorotArticle = document.getElementById("sorotArticle");
+  const myArticles = document.getElementById("myArticles");
+  console.log(sorotArticle);
+  console.log(myArticles);
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.slice(0, 2).forEach((item) => {
+        const card = createCard(
+          item.image,
+          item.title,
+          item.description
+        );
+        sorotArticle.appendChild(card);
+      });
+      data.slice(2).forEach((item) => {
+        const cardPilihan = createCardPilihan(
+          item.image,
+          item.createdAt,
+          item.title
+        );
+        myArticles.appendChild(cardPilihan);
+      });
+    });
+});
