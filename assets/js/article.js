499821cfff8c61a6f7948d86048561b4002d2a9b
Wahdan Najmil <najmilwahdan@gmail.com>
2023-05-04 17:07:22 +0700

    add detailArticle

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,141 +1,161 @@
-const url = "https://644e64ed1b4567f4d5866c65.mockapi.io/articles";
+const url = "https://644e64ed1b4567f4d5866c65.mockapi.io/articles/";
 
-function createCard(image, title, description) {
-  const card = document.createElement("div");
-  card.classList.add("card", "bg-transparent");
+// Function untuk mengambil data artikel dari API
+async function getArticles(page) {
+  try {
+    const response = await fetch(`${url}?page=${page}&limit=6`);
+    const data = await response.json();
 
-  const img = document.createElement("img");
-  img.src = image;
-  img.classList.add("card-img-top");
-  img.alt = "img-artikel";
-
-  const body = document.createElement("div");
-  body.classList.add("card-body");
-
-  const titleH5 = document.createElement("h5");
-  titleH5.classList.add("card-title", "fw-semibold");
-  titleH5.style = "font-size: 18px; padding-bottom: 10px";
-  titleH5.textContent = title;
-
-  const paragraf = document.createElement("p");
-  paragraf.classList.add("card-text", "fw-normal");
-  paragraf.style = "font-size: 14px; color: #696969";
-  paragraf.textContent = description;
-
-  const button = document.createElement("div");
-  button.classList.add("d-flex", "justify-content-end");
-
-  const anchor = document.createElement("a");
-  anchor.classList.add("btn", "py-3", "px-3");
-  anchor.textContent = "Baca Selengkapnya";
-
-  button.appendChild(anchor);
-
-  body.appendChild(titleH5);
-  body.appendChild(paragraf);
-  body.appendChild(button);
-
-  card.appendChild(img);
-  card.appendChild(body);
-
-  console.log(card);
-  return card;
-}
-
-function createCardPilihan(image, createdAt, title) {
-  const row = document.createElement("div");
-  row.classList.add("row", "mb-3");
-
-  const colImg = document.createElement("div");
-  colImg.classList.add("col-md-6");
-
-  const img = document.createElement("img");
-  img.src = image;
-  img.classList.add("img-fluid", "rounded-start");
-  img.alt = "img-artikel";
-  
-  colImg.appendChild(img);
-
-  const colText = document.createElement("div");
-  colText.classList.add("col-md-6");
-
-  const body = document.createElement("div");
-  body.classList.add("card-body");
-
-  const dateSpan = document.createElement("span");
-  dateSpan.classList.add("small", "text-muted");
-  dateSpan.textContent = createdAt
-    .split("T")[0]
-    .split("-")
-    .slice(0, 3)
-    .join("-");
-
-  const titleH5 = document.createElement("h5");
-  titleH5.classList.add("card-title", "fw-bold");
-  titleH5.style.fontSize = "16px";
-  titleH5.textContent = title;
-
-  body.appendChild(dateSpan);
-  body.appendChild(titleH5);
-
-  colText.appendChild(body);
-
-  row.appendChild(colImg);
-  row.appendChild(colText);
-
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.appendChild(row);
-
-  return card;
-}
+    
+const sorotArticle = document.getElementById("sorotArticle");
+sorotArticle.innerHTML = `
+  <div class="col">
+    <div class="row-md-6 mb-3">
+      <div class="card h-100">
+        <img
+          src="${data[0].image}"
+          class="card-img-top"
+          alt="${data[0].title}"
+        />
+        <div class="card-body">
+          <h2 class="card-title fw-bold mb-3">${data[0].title}</h2>
+          <p class="card-text">${data[0].description}</p>
+          <a href="articleDetail.html?id=${data[0].id}" class="btn btn-success">Baca Selengkapnya</a>
+        </div>
+      </div>
+    </div>
+    <div class="row-md-6 mb-3">
+      <div class="card h-100">
+        <img
+          src="${data[1].image}"
+          class="card-img-top"
+          alt="${data[1].title}"
+        />
+        <div class="card-body">
+          <h2 class="card-title fw-bold mb-3">${data[1].title}</h2>
+          <p class="card-text">${data[1].description}</p>
+          <a href="articleDetail.html?id=${data[1].id}" class="btn btn-success">Baca Selengkapnya</a>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  const sorotArticle = document.getElementById("sorotArticle");
-  const myArticles = document.getElementById("myArticles");
-  console.log(sorotArticle);
-  console.log(myArticles);
+    // Menampilkan artikel pilihan
+    const myArticles = document.getElementById("myArticles");
+    myArticles.innerHTML = "";
+    for (let i = 1; i < data.length; i++) {
+      myArticles.innerHTML += `
+        <div class="col-md-12">
+          <div class="card">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="${data[i].image}" class="img-fluid rounded-start" alt="${data[i].title}"/>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body" style="display: flex; align-items: center;">
+                  <div>
+                    <h5 class="card-title fw-bold"style="font-size: 20px">${data[i].title}</h5>
+                    <a href="articleDetail.html?id=${data[i].id}" class="btn btn-success">Baca Selengkapnya</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.slice(0, 2).forEach((item) => {
-        const card = createCard(
-          item.image,
-          item.title,
-          item.description
-        );
-        sorotArticle.appendChild(card);
-      });
-      data.slice(2).forEach((item) => {
-        const cardPilihan = createCardPilihan(
-          item.image,
-          item.createdAt,
-          item.title
-        );
-        myArticles.appendChild(cardPilihan);
+
+
+      `;
+    }
+  // Menambahkan event listener ke tombol Read More
+    const readMoreButtons = document.querySelectorAll(".read-more");
+    readMoreButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
+        const articleId = event.target.getAttribute("data-id");
+        getArticleById(articleId);
       });
     });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Function untuk mengambil data artikel berdasarkan ID
+async function getArticleById(id) {
+  try {
+    const response = await fetch(`${url}/${id}`);
+    const data = await response.json();
+
+    // Menampilkan data artikel pada elemen HTML dengan id articleDetail
+    const articleDetail = document.getElementById("articleDetail");
+    articleDetail.innerHTML = `
+      <div class="card mb-5">
+        <img src="${data.image}" class="card-img-top" alt="${data.title}">
+        <div class="card-body"><br>
+          <h2 class="card-title" >${data.title}</h2><br>
+          <p class="card-text" style="font-size: 18px">${data.article}</p>
+          <p class="card-text"><small class="text-muted">${data.author}</small></p>
+        </div>
+      </div>
+    `;
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Memanggil fungsi getArticleById() saat halaman selesai di-load
+window.addEventListener("load", () => {
+  // Mendapatkan id artikel dari URL parameter
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("id");
+
+  // Memanggil fungsi getArticleById() dengan id artikel sebagai argumen
+  getArticleById(id);
 });
 
-// Mendapatkan referensi ke tautan halaman
-const page1Link = document.getElementById('page1');
-const page2Link = document.getElementById('page2');
-const page3Link = document.getElementById('page3');
-
-// Menambahkan event listener untuk setiap tautan halaman
-page1Link.addEventListener('click', function(event) {
-  event.preventDefault(); // Menghentikan perilaku bawaan dari tautan
-  window.location.href = page1Link.getAttribute('index.html'); // Mengarahkan ke halaman yang ditentukan di atribut href
+// Memanggil function getArticles() saat halaman selesai di-load
+window.addEventListener("load", () => {
+  getArticles(1);
 });
 
-page2Link.addEventListener('click', function(event) {
-  event.preventDefault();
-  window.location.href = page2Link.getAttribute('forum.html');
-});
+// menambahkan event listener pada tombol Next Article
+  document.getElementById("nextArticleButton").addEventListener("click", function() {
+    // mengambil id artikel yang sekarang ditampilkan
+    const currentId = parseInt(window.location.search.replace("?id=", ""));
+    // mengambil id artikel selanjutnya
+    const nextId = currentId + 1;
+    if (nextId == 10) {
+    // jika masih ada artikel sebelumnya
+    // redirect ke halaman detail artikel sebelumnya
+      document.getElementById("nextArticleButton").style.display = "none";
+    
+  } else {
+    // jika artikel yang ditampilkan adalah artikel pertama (index 0)
+    // sembunyikan tombol "Prev Article"
+    window.location.href = "articleDetail.html?id=" + nextId;
+  }
+  });
 
-page3Link.addEventListener('click', function(event) {
-  event.preventDefault();
-  window.location.href = page3Link.getAttribute('index.html');
+// menambahkan event listener pada tombol Next Article
+  document.getElementById("prevArticleButton").addEventListener("click", function() {
+    // mengambil id artikel yang sekarang ditampilkan
+    const currentId = parseInt(window.location.search.replace("?id=", ""));
+    // mengambil id artikel selanjutnya
+    const prevId = currentId - 1;
+
+    if (prevId == 0) {
+    // jika masih ada artikel sebelumnya
+    // redirect ke halaman detail artikel sebelumnya
+      document.getElementById("prevArticleButton").style.display = "none";
+    
+  } else {
+    // jika artikel yang ditampilkan adalah artikel pertama (index 0)
+    // sembunyikan tombol "Prev Article"
+    window.location.href = "articleDetail.html?id=" + prevId;
+  }
 });
+  
--- assets/js/loginRegister.js
+++ assets/js/loginRegister.js
@@ -1,81 +1,89 @@
-const apiUrl = 'https://645072b0e1f6f1bb2297ddb1.mockapi.io/api/v1/user';
+const apiUrl = 'https://6450b0c5a3221969114f68c0.mockapi.io/api/loginRegister/users';
+const isLoggedIn = localStorage.getItem('isLoggedIn');
 
-        // fungsi untuk mengecek apakah email sudah terdaftar
-        function isEmailRegistered(email) {
-            return fetch(apiUrl)
-                .then(response => response.json())
-                .then(users => users.some(user => user.email === email));
-        }
+function isEmailRegistered(email) {
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(users => users.some(user => user.email === email));
+}
 
-        // fungsi untuk registrasi user
-        function registerUser(fullname, name, email, password) {
-            return fetch(apiUrl, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ fullname, name, email, password })
-            });
-        }
+function registerUser(fullname, name, email, password) {
+  return fetch(apiUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ fullname, name, email, password })
+  });
+}
 
-        // meng-handle submit form registrasi
-        const registerForm = document.getElementById('register-form');
-        registerForm.addEventListener('submit', event => {
-            event.preventDefault();
-            const fullname = document.getElementById('register-fullname').value;
-            const name = document.getElementById('register-name').value;
-            const email = document.getElementById('register-email').value;
-            const password = document.getElementById('register-password').value;
-            isEmailRegistered(email)
-                .then(emailRegistered => {
-                    if (emailRegistered) {
-                        alert('Email is already registered');
-                    } else {
-                        registerUser(fullname, name, email, password)
-                            .then(response => {
-                                if (response.ok) {
-                                    alert('Registration successful');
-                                    registerForm.reset();
-                                } else {
-                                    alert('Registration failed');
-                                }
-                            })
-                            .catch(error => console.error(error));
-                    }
-                })
-                .catch(error => console.error(error));
-        });
+// function showNavbar() {
+//   document.getElementById("user-navbar").style.display = "block";
+//   document.getElementById("button-login").style.display = "none";
+// }
 
-        // meng-handle submit form login
-        const loginForm = document.getElementById('login-form');
-        loginForm.addEventListener('submit', event => {
-            event.preventDefault();
-            const email = document.getElementById('login-email').value;
-            const password = document.getElementById('login-password').value;
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(users => {
-                    const user = users.find(user => user.email === email && user.password === password);
-                    if (user) {
-                        window.location.href = "index.html";
-                        alert(`Welcome, ${user.email}!`);
-                        loginForm.reset();
-                    } else {
-                        alert('Invalid email or password');
-                    }
-                })
-                .catch(error => console.error(error));
-        });
+const registerForm = document.getElementById('register-form');
+registerForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const fullname = document.getElementById('register-fullname').value;
+  const name = document.getElementById('register-name').value;
+  const email = document.getElementById('register-email').value;
+  const password = document.getElementById('register-password').value;
+  isEmailRegistered(email)
+    .then(emailRegistered => {
+      if (emailRegistered) {
+        alert('Email is already registered');
+      } else {
+        registerUser(fullname, name, email, password)
+          .then(response => {
+            if (response.ok) {
+                alert('Registration successful');
+                showNavbar()
+              registerForm.reset();
+            } else {
+              alert('Registration failed');
+            }
+          })
+          .catch(error => console.error(error));
+      }
+    })
+    .catch(error => console.error(error));
+});
 
-        $(document).ready(function() {
-            $('#show-register-form').click(function() {
-                $('#login-form-container').hide();
-                $('#register-form-container').show();
-            });
-        });
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(users => {
+      const user = users.find(user => user.email === email && user.password === password);
+      if (user) {
+        localStorage.setItem('isLoggedIn', 'true'); // simpan status login user
+        window.location.href = "index.html";
+        alert(`Welcome, ${user.email}!`);
+        loginForm.reset();
+      } else {
+        alert('Invalid email or password');
+      }
+    })
+    .catch(error => console.error(error));
+});
 
-        $(document).ready(function() {
-            $('#show-login-form').click(function() {
-                $('#login-form-container').show();
-                $('#register-form-container').hide();
-            });
-        });
-    
+$(document).ready(function() {
+  $('#show-register-form').click(function() {
+    $('#login-form-container').hide();
+    $('#register-form-container').show();
+  });
+});
+
+$(document).ready(function() {
+  $('#show-login-form').click(function() {
+    $('#login-form-container').show();
+    $('#register-form-container').hide();
+  });
+});
+
+// panggil fungsi showNavbar() jika user sudah login
+// if (isLoggedIn === 'true') {
+//   showNavbar();
+// }
